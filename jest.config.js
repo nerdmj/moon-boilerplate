a385bb027460ed1f3576096f6e26fff0814243d3
nerdmj <er.manishjatwani@gmail.com>
2018-05-11 17:06:17 +0530

    Boilerplate created with test env. Browser sync is remaining.

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+   "setupFiles": ["babel-polyfill"],
+   transform: {
+   "\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
+ },
+ bail: false,
+ collectCoverageFrom: [
+   'src/**/*.{js,jsx}',
+   "!**/node_modules/**",
+   "!**/vendor/**"
+ ],
+ coverageDirectory: "<rootDir>/coverage", // [string]
+ moduleNameMapper: {
+   "\\.(css|less|styl|scss|sass|sss)$": "identity-obj-proxy",
+ },
+ snapshotSerializers: [
+     "enzyme-to-json/serializer"
+ ],
+ globals: {
+   __DEV__: true,
+ },
+ "setupTestFrameworkScriptFile": "<rootDir>/enzyme.setup.js",
+ "setupFiles": [
+   "raf/polyfill"
+ ]
+}
